818129e1081540ff013a74dc9686c94859a80ca4
Rendy Ramadhana <rendy.rw.rw@gmail.com>
2025-05-05 11:34:05 +0700

    Deploy Astro site

---
+++ src/content/blog/second-post.md
@@ -0,0 +1,24 @@
+
+---
+title: Judul Post
+description: Ringkasan post
+pubDate: 2025-05-04
+thumbnail: /src/images/blogs/adc.png
+tag: Tutorial
+author: rendy
+---
+
+
+
+## Welcome to the Second Post
+
+This is the second post in the series. Stay tuned for more updates and exciting content!
+
+### Highlights
+
+- Continuation of the journey
+- New insights and ideas
+- More to come!
+
+Thank you for reading!
+
---
+++ src/data/blog.js
@@ -0,0 +1,27 @@
+export const BlogList = [
+  {
+    src: '/src/images/blogs/adc.png',
+    title: 'Implementation of the ADS1232 24-bit External ADC in Geophone Sensor Applications',
+    description: 'The communication between peripherals on the Wemos board and the ADS1232 IC is based on the I2C protocol. This communication involves multiple signals operating in sync. In this context, the peripherals require a clock signal as a time reference, which is essential for the calibration process and for transmitting the 24-bit conversion data from the ADS1232 IC.',
+    date: '2024-10-02',
+    link: '/blog/mengenal-astro',
+    tag: 'ADC; I2C; ADS1232; Geophone'
+  },
+  {
+    src: '/src/images/blogs/lora.png',
+    title: 'Assessment of LoRa Applications in Microcontroller Systems',
+    description: 'Testing was conducted to determine the maximum transmission and reception range of LoRa signals by sending simple string data. The experiments utilized two types of antennas: omnidirectional and Yagi.',
+    date: '2024-09-19',
+    link: '/blog/dark-mode-astro',
+    tag: 'LoRa; Wireless; ESP32; IoT'
+  },
+  {
+    src: '/src/images/blogs/mysql.png',
+    title: 'Optimizing Data Storage Through the Use of Online Databases',
+    description: 'The communication between the Wemos board and the ADS1232 IC is established using the I2C protocol, which relies on a synchronized clock signal. This clock signal plays a critical role in ensuring precise calibration and the reliable transmission of 24-bit conversion data from the IC.',
+    date: '2022-08-01',
+    link: '/blog/astro-framework',
+    tag: 'database; MySQL; Geophysics Datase'
+  },
+  // ...dst
+]
